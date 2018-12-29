5dc360c0e367188d56271f23317834678df3c3c3
mario-mitchell <mario.mitchell8291@gmail.com>
2018-12-29 13:37:38 -0800

    part 1 almost done

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-let friends = ["Nathalie","Benal", "Gabe", "David", "Javon"];
+let friends = ["Nathalie", "Benal", "Gabe", "David", "Javon"];
 
-for (var i = 0; i < friends.length; i++ ) { console.log(friends[i]);
-    for (var j = 0; j< friends.length; j++) console.log(friends[j]);
-    //if (friends[i] === friends[j]){
-    console.log("99 lines of code in the file, 99 lines of code;" + friends[i] + " strikes one out, clears it all out,");
+for (var i = 0; i < friends.length; i++) {
+
+    console.log(friends[i]);
+    for (var j = 99; j > 0; j--) {
+
+        if (j == 1) {
+            console.log(j + " Line of code on the wall " + j + " Line of code " + friends[i] + " STRIKES one down clears it all out no more lines of code on the wall");
+        } else {
+            console.log(j + " Lines of code on the wall " + j + " Lines of code " + friends[i] + " STRIKES one down clears it all out " + (j - 1) + " on the wall");
+        }
     }
-//}
+}
