bdd1149c82a24c78370cedb8cf33f258a71ab462
mario-mitchell <mario.mitchell8291@gmail.com>
2018-12-29 13:04:54 -0800

    please work

---
+++ app.js
@@ -0,0 +1,8 @@
+let friends = ["Nathalie","Benal", "Gabe", "David", "Javon"];
+
+for (var i = 0; i < friends.length; i++ ) { console.log(friends[i]);
+    for (var j = 0; j< friends.length; j++) console.log(friends[j]);
+    //if (friends[i] === friends[j]){
+    console.log("99 lines of code in the file, 99 lines of code;" + friends[i] + " strikes one out, clears it all out,");
+    }
+//}
